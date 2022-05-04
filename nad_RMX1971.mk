7bbb5ea56a9ee9f82f117ca85e9ccde329afbe9b
703joko <703joko@gmail.com>
2022-05-03 22:42:30 -0400

    May FP SecPatch updated

--- nad_RMX1971.mk
+++ nad_RMX1971.mk
@@ -28,8 +28,8 @@ TARGET_SUPPORTS_QUICK_TAP := true
 PRODUCT_GMS_CLIENTID_BASE := android-realme
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="raven-user 12 SP2A.220405.004 8233519 release-keys"
-    BUILD_FINGERPRINT="google/raven/raven:12/SP2A.220405.004/8233519:user/release-keys"
+    PRIVATE_BUILD_DESC="raven-user 12 SP2A.220505.002 8353555 release-keys"
+    BUILD_FINGERPRINT="google/raven/raven:12/SP2A.220505.002/8353555:user/release-keys"
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.build.fingerprint=$(BUILD_FINGERPRINT)
