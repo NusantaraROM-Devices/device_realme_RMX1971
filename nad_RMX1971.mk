c1dc40b8b467281714e492be9bd0984419c8d9ce
703joko <703joko@gmail.com>
2022-07-14 02:29:35 -0400

    RMX1971: July FP updated

--- nad_RMX1971.mk
+++ nad_RMX1971.mk
@@ -27,8 +27,8 @@ PRODUCT_MANUFACTURER := Realme
 PRODUCT_GMS_CLIENTID_BASE := android-realme
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="raven-user 12 SP2A.220505.002 8353555 release-keys"
-    BUILD_FINGERPRINT="google/raven/raven:12/SP2A.220505.002/8353555:user/release-keys"
+    PRIVATE_BUILD_DESC="raven-user 12 SQ3A.220705.003.A1 8672226 release-keys"
+    BUILD_FINGERPRINT="google/raven/raven:12/SQ3A.220705.003.A1/8672226:user/release-keys"
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.build.fingerprint=$(BUILD_FINGERPRINT)
