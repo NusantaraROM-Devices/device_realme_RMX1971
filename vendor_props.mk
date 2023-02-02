6b8d5a0c22c64ce14916cba7ddee9795f9d313bf
ghostrider-reborn <gh0strider.2k18.reborn@gmail.com>
2023-02-02 19:50:08 +0530

    sdm710-common: props: Enable display protected contents in SF
     - required for pip in netflix and other widevine apps
     - stock sets this too
    
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Signed-off-by: improbir <ghosh_probir@yahoo.com>
    Signed-off-by: kssrao13882 <kssrao13882@gmail.com>

--- vendor_props.mk
+++ vendor_props.mk
@@ -103,6 +103,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     debug.hwui.target_cpu_time_percent=30 \
     ro.config.avoid_gfx_accel=true \
     debug.sf.disable_client_composition_cache=1 \
+    ro.surface_flinger.protected_contents=true \
     ro.hardware.vulkan=adreno \
     ro.hardware.egl=adreno
 
